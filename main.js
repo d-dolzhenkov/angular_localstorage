5ce9d0c773994673d49d5a0ac2d36cb412efc4ec
d-dolzhenkov <dolzhenkovdanil@gmail.com>
2016-10-07 11:09:25 +0300

    no message

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-var module = angular.module("myApp", ['ngStorage'])
-                    .controller("MyController", MyController)
-                    .service("myService", MyService);
 
-MyController.$inject = ["$scope", "myService"];
 
-function MyController($scope, $localStorage, myService) {
-    $scope.$storage = $localStorage.userData = {
+angular.module("myApp", ['ngStorage'])
+    .controller("MyController", MyController);
+
+MyController.$inject = ["$scope", "$localStorage"];
+
+function MyController($scope, $localStorage) {
+    $scope.$storage = $localStorage.$default({});
+
+    $scope.userData = {
         userName: "",
         age: ""
     };
-
-
-    $scope.onSave = function () {
-        myService.saveData(userData);
-    }
-}
-
-function MyService() {
-
-    this.saveData = function (data) {
-
-
-        /*$http({
-            method: "POST",
-            data: data
-        })*/
-    }
 }
 
 angular.bootstrap(angular.element(document), ['myApp']);
